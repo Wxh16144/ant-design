a5b7b665c28c89ec28f626019de8c3b9159e9104
wuxh <wxh1220@gmail.com>
2023-10-18 18:36:27 +0800

    deps: bump rc-upload@4.4.0

--- components/upload/Upload.tsx
+++ components/upload/Upload.tsx
@@ -352,7 +352,12 @@ const InternalUpload: React.ForwardRefRenderFunction<UploadRef, UploadProps> = (
     disabled: mergedDisabled,
     beforeUpload: mergedBeforeUpload,
     onChange: undefined,
-    hasControlInside,
+    styles: {
+      input: { display: 'unset', visibility: 'hidden' },
+    },
+    classNames: {
+      input: `${prefixCls}-trigger`,
+    },
   } as RcUploadProps;
 
   delete rcUploadProps.className;
